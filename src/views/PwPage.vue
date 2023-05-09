7c45a670c610d2c5c3269320336b703f95366ad2
99yang <86218573+99yang@users.noreply.github.com>
2023-05-10 05:26:53 +0900

    PwPage: 오류 수정

--- src/views/PwPage.vue
+++ src/views/PwPage.vue
@@ -1,18 +1,22 @@
 <template>
   <body>
     <section id="signUp">
-      <form id="loginForm" v-on:submit.prevent="pwForm">
+      <form v-on:submit.prevent="pwSearchForm" id="loginForm">
         <h2 class="home__description">비밀번호 찾기</h2>
         <!-- v-model: userEmail을 양방향 데이터 바인딩으로 연결 -->
         <input
+          id="userEmail"
+          v-model="userEmail"
           type="text"
           placeholder="이메일 주소"
           class="loginForm__text"
-          v-model="userEmail"
+          :class="{ invalid: !emailValid }"
         /><br />
         <div v-if="!emailValid">이메일 주소를 올바르게 입력해주세요.</div>
         <p>해당 이메일 주소로 비밀번호 재설정 링크를 보내드립니다.<br /></p>
-        <button v-if="isFormValid" class="loginForm__button">전송</button>
+        <button v-if="isFormValid" class="loginForm__button" value="">
+          전송
+        </button>
       </form>
     </section>
   </body>
@@ -22,7 +26,6 @@
 import axios from 'axios';
 
 export default {
-  name: 'PwPage',
   data() {
     return {
       userEmail: '',
@@ -37,29 +40,30 @@ export default {
     isFormValid() {
       return this.emailValid;
     },
+  },
 
-    methods: {
-      pwForm() {
-        // pwForm 유효성 검사
-        if (this.isFormValid) {
-          axios
-            .post('http://127.0.0.1:3000/accounts/pwsearch', {
-              userEmail: this.userEmail,
-            })
-            .then(res => {
-              if (res.data.emailSystemResult == 1) {
-                alert('존재하지 않는 이메일 입니다.');
-              } else if (res.data.signUpResult == -1) {
-                alert('backend error: SQL err');
-              } else if (res.data.signUpResult == 0) {
-                alert('이메일 전송 성공');
-                this.$router.push('/');
-              } else {
-                alert('backend error: err');
-              }
-            });
-        }
-      },
+  methods: {
+    pwSearchForm() {
+      // pwSearchForm 유효성 검사
+      if (this.isFormValid) {
+        console.log(this.userEmail);
+        axios
+          .post('http://127.0.0.1:3000/accounts/pwsearch', {
+            userEmail: this.userEmail,
+          })
+          .then(res => {
+            if (res.data.emailSystemResult == 1) {
+              alert('존재하지 않는 이메일 입니다.');
+            } else if (res.data.emailSystemResult == -1) {
+              alert('backend error: SQL err');
+            } else if (res.data.emailSystemResult == 0) {
+              alert('이메일 전송 성공');
+              this.$router.push('/');
+            } else {
+              alert('backend error: err');
+            }
+          });
+      }
     },
   },
 };
